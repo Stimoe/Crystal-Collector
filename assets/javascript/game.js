246855abef9d796a44b4f412b94e8a50e71b996e
Stimoe <stimoe3@gmail.com>
2019-09-04 17:26:37 -0700

    game working with css

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,51 +5,71 @@ var rock1;
 var rock2;
 var rock3;
 var rock4;
-var userNumber=0;
-var newNumber
+var userNumber = 0;
+var newNumber;
+var wins = 0;
+var losses = 0;
+var currentValue;
 
 
-function gameReset (){
-computerNumber = Math.floor((Math.random() * 100) + 20);
-console.log(computerNumber);
-rock1 = Math.floor((Math.random() * 12) + 1);
-$("#rock1").val(rock1);
-rock2 = Math.floor((Math.random() * 12) + 1);
-rock3 = Math.floor((Math.random() * 12) + 1);
-rock4 = Math.floor((Math.random() * 12) + 1);
-console.log(rock1)
-console.log(rock2)
-console.log(rock3)
-console.log(rock4)
+function gameReset() {
+    computerNumber = Math.floor((Math.random() * 100) + 20);
+    console.log(computerNumber);
+    rock1 = Math.floor((Math.random() * 12) + 1);
+    rock2 = Math.floor((Math.random() * 12) + 1);
+    rock3 = Math.floor((Math.random() * 12) + 1);
+    rock4 = Math.floor((Math.random() * 12) + 1);
+    console.log(rock1);
+    console.log(rock2);
+    console.log(rock3);
+    console.log(rock4);
+    userNumber=0;
+    $("#number").text(computerNumber);
+    $("#current_number_span").text(userNumber);
 }
-
-$(document).ready(function() {
-    $("#number").text(computerNumber)
-        $("#rock1").on("click", function() {
-            newNumber= userNumber + rock1
-        var currentValue = parseInt($("#current_number_span").text())
-        $("#current_number_span").text(currentValue + newNumber)
-            
-        })
-            $("#rock2").on("click", function() {
-                newNumber= userNumber + rock2
-            var currentValue = parseInt($("#current_number_span").text())
-            $("#current_number_span").text(currentValue + newNumber)
-
-            })
-                $("#rock3").on("click", function() {
-                    newNumber= userNumber + rock3
-                var currentValue = parseInt($("#current_number_span").text())
-                $("#current_number_span").text(currentValue + newNumber)
-
-                })
-                    $("#rock4").on("click", function() {
-                        newNumber= userNumber + rock4
-                    var currentValue = parseInt($("#current_number_span").text())
-                    $("#current_number_span").text(currentValue + newNumber)
-
-                    })      
+function check(currentValue, computerNumber) {
+    if (currentValue === computerNumber) {
+        wins++
+        $("#wins").text(wins);
+        alert("You Won!!");
+        gameReset();
+    }
+    else if (currentValue > computerNumber) {
+        losses++
+        $("#losses").text(losses);
+        alert("You Lost");
+        gameReset();
+    }
+}
+$(document).ready(function () {
+    $("#rock1").on("click", function () {
+        newNumber = userNumber + rock1;
+        currentValue = parseInt($("#current_number_span").text());
+        currentValue = (currentValue + newNumber);
+        $("#current_number_span").text(currentValue);
+        console.log(currentValue);
+        check(currentValue, computerNumber);
+    })
+    $("#rock2").on("click", function () {
+        newNumber = userNumber + rock2
+        currentValue = parseInt($("#current_number_span").text())
+        currentValue = (currentValue + newNumber);
+        $("#current_number_span").text(currentValue)
+        check(currentValue, computerNumber);
+    })
+    $("#rock3").on("click", function () {
+        newNumber = userNumber + rock3
+        currentValue = parseInt($("#current_number_span").text())
+        currentValue = (currentValue + newNumber);
+        $("#current_number_span").text(currentValue)
+        check(currentValue, computerNumber);
+    })
+    $("#rock4").on("click", function () {
+        newNumber = userNumber + rock4
+        currentValue = parseInt($("#current_number_span").text())
+        currentValue = (currentValue + newNumber);
+        $("#current_number_span").text(currentValue)
+        check(currentValue, computerNumber);
+    })
 })
-
-
 gameReset();
